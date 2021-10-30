bd7cb9fae8cb56756ac461ba8e83af93a89490b7
Apowmet <4ai6ezcaxapa@gmail.com>
2021-10-30 23:07:48 +0300

    веб-приложение

--- infrastructure/Shop.Memory/BookRepository.cs
+++ infrastructure/Shop.Memory/BookRepository.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 namespace Shop.Memory
 {
-    public class Class1
+    public class BookRepository : IBookRepository
     {
+        private readonly Book[] books = new[]
+        {
+            new Book (1, "Шерлок Холмс"),
+            new Book (2, "Гарри Поттер"),
+            new Book (3, "Ведьмак"),
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                .ToArray();
+        }
     }
 }
